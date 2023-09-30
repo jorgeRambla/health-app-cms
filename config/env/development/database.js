8e09559765ff5211cd4083d682a6c64decf66988
Jorge Rambla <yorch044zgz@gmail.com>
2023-10-01 00:22:55 +0200

    v3

--- config/env/development/database.js
+++ config/env/development/database.js
@@ -1,10 +1,10 @@
 module.exports =  ({ env }) => ({
 	connection: {
-		client: 'mysql',
+		client: 'postgres',
 		connection: {
 		host: env('DATABASE_HOST', 'localhost'),
 			port: env.int('DATABASE_PORT', 1234),
-			database: env('DATABASE_NAME', 'strapi'),
+			database: env('DATABASE_NAME', 'postgres'),
 			user: env('DATABASE_USERNAME', 'strapi'),
 			password: env('DATABASE_PASSWORD', 'strapi'),
 			ssl: env.bool('DATABASE_SSL', false)
